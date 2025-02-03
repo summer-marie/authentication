839b477c61e69843c96265d09fddeecfdb1e8dd0
summer halsey <shalsey@perseverenow.org>
2025-02-03 11:40:57 -0700

    passport stuff

--- server/index.js
+++ server/index.js
@@ -1,17 +1,25 @@
+import "dotenv/config";
 import express from "express";
+import cors from 'cors'
+import passport from 'passport';
+import './strategies/local-strategy.js' 
 
-
-
-const app = express()
-const port = 8000
-
-
-
+const app = express();
+app.use(express.json())
+app.use(cors())
+const port = 8000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Your Server says Hello!!')
 })
 
+app.use(passport.initialize())
+app.use(passport.session())
+
+// app.use(routes)
+
+app.post('/api/auth', passport.authenticate('local'), (request, respnse) => {})
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Template app listening on port ${port}`)
 })
---
+++ server/utils/constants.js
@@ -0,0 +1,9 @@
+export const mockUsers = [
+  { id: 1, username: "summer", displayName: "Summer", password: "test123" },
+  { id: 2, username: "jack", displayName: "Jack", password: "test124" },
+  { id: 3, username: "sally", displayName: "Sally", password: "test125" },
+  { id: 4, username: "may", displayName: "May", password: "test126" },
+  { id: 5, username: "june", displayName: "Juna", password: "test127" },
+  { id: 6, username: "july", displayName: "July", password: "test128" },
+  { id: 7, username: "april", displayName: "April", password: "test129" },
+];
