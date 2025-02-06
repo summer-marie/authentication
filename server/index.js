871a9cbfc7e6d87f96c6b80052bb968321200d90
summer halsey <shalsey@perseverenow.org>
2025-02-06 09:22:15 -0700

    Deleted express code and refactored.

--- server/index.js
+++ server/index.js
@@ -1,25 +1,20 @@
-import "dotenv/config";
-import express from "express";
-import cors from 'cors'
-import passport from 'passport';
-import './strategies/local-strategy.js' 
+import "dotenv/config"
+import express from "express"
+import session from "express-sessions"
+import cors from "cors"
+import passport from "passport"
+import "./strategies/local-strategy.js"
 
 const app = express();
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res.send('Your Server says Hello!!')
-})
 
-app.use(passport.initialize())
-app.use(passport.session())
-
-// app.use(routes)
-
-app.post('/api/auth', passport.authenticate('local'), (request, respnse) => {})
+app.get("/", (req, res) => {
+  res.send("Your Server says Hello!!");
+});
 
 app.listen(port, () => {
-  console.log(`Template app listening on port ${port}`)
-})
+  console.log(`Template app listening on port ${port}`);
+});
