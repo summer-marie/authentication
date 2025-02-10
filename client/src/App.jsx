8889aae45e5700d0cc106ab4e3d7cc982dbc23e9
summer halsey <shalsey@perseverenow.org>
2025-02-10 09:06:11 -0700

    Serialize user

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,67 @@
 import './App.css'
 
 function App() {
-  return <>Hello Template</>
+  return (
+    <>
+
+      <div className="mt-20">
+        <h2 className="mt-10 mb-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900 dark:text-sky-500">
+          Sign in to your account
+        </h2>
+        <form
+          className="flex flex-col items-center justify-center"
+          // onSubmit={handleSubmit}
+        >
+          <div className="mb-5">
+            <label
+              htmlFor="email"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your email
+            </label>
+            <input
+              // value={loginForm.email}
+              // onChange={(e) =>
+              //   setLoginForm({ ...loginForm, email: e.target.value })
+              // }
+              type="email"
+              id="email"
+              className="w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              placeholder="name@flowbite.com"
+              required
+            />
+          </div>
+          <div className="mb-5">
+            <label
+              htmlFor="password"
+              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Your password
+            </label>
+            <input
+              // value={loginForm.password}
+              // onChange={(e) =>
+              //   setLoginForm({ ...loginForm, password: e.target.value })
+              // }
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              className="w-80 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            className="text-white bg-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-sky-300 font-medium rounded-lg text-sm w-80 px-5 py-2.5 text-center dark:bg-sky-600 dark:hover:bg-sky-700 dark:focus:ring-sky-800"
+            // disabled={loading}
+          >
+            Sign in
+          </button>
+        </form>
+      </div>
+    </>
+  )
 }
 
 export default App
--- server/strategies/localStrategy.js
+++ server/strategies/localStrategy.js
@@ -1,14 +1,35 @@
 import passport from 'passport'
 import { Strategy } from 'passport-local'
+import userModel from '../user/userModel.js'
+
+passport.serializeUser((user, done) => {
+  console.log('serializeUser', user)
+  done(null, user.id)
+})
+
+passport.deserializeUser(async (id, done) => {
+  console.log('deserializedUser', id)
+  try {
+    // Both work, so i kept them both for ref.
+    // const findUser = await userModel.findById(id)
+    const findUser = await userModel.findOne({ _id: id })
+    if (!findUser) {
+      throw new Error('Invalid credentials')
+    }
+    done(null, findUser)
+  } catch (err) {
+    done(err, null)
+  }
+})
 
 export default passport.use(
   // passing in my instance
-  new Strategy({ usernameField: 'email' }, (username, password, done) => {
-    console.log("username", username, "password", password)
+  new Strategy({ usernameField: 'email' }, async (username, password, done) => {
+    console.log('username', username, 'password', password)
     // search users in db
     try {
-      // const findUser = userModel.findOne({ email: username })
-
+      const findUser = await userModel.findOne({ email: username })
+      console.log('findUser', findUser)
       // no user found - err
       if (!findUser) {
         throw new Error('user not found')
