0ed20df7c19bcf0264b17affbc2aaf460d1579db
summer halsey <shalsey@perseverenow.org>
2025-02-11 09:44:35 -0700

    Adds passport to user create route

--- server/user/userIndex.js
+++ server/user/userIndex.js
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import passport from "passport"
 import userCreate from './userCreate.js'
 
 const userRouter = Router()
 
-userRouter.post('/', userCreate)
+userRouter.post('/',  passport.authenticate("jwt", { session: false }), userCreate)
 
 export default userRouter
